f77a06e72a544aa5b7a565121b1a8bed612e08ba
quanm200699 <maplestory2012vn@gmail.com>
2021-07-27 09:13:12 +0700

    product list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProductListComponent} from "./product/product-list/product-list.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
